c5fcff3707063ba3e29491235cc5a4c0924fbcb9
Tonja Heinemann <tonja.heinemann@isw.uni-stuttgart.de>
2022-07-29 18:00:24 +0200

    added own server to start

--- milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/SimpleClient.java
+++ milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/SimpleClient.java
@@ -4,18 +4,38 @@ import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
 import org.eclipse.milo.opcua.stack.core.Identifiers;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.examples.server.ExampleServer;
 
 public class SimpleClient {
-    public static void main(String[] args) {
-        SimpleClient simple = new SimpleClient();
 
+    private ExampleServer exampleServer;
+    private String eUrl;
+
+    public static void main(String[] args) {
+        
+
+        SimpleClient simple = new SimpleClient();
+        
+        simple.eUrl = "opc.tcp://localhost"; // if server is running locally
+        try {
+            simple.start_server();
+            simple.eUrl = "opc.tcp://localhost:12686/milo";
+        } catch (Exception e) {
+            System.out.println("Exception" + e.getMessage());
+        }
+        
         simple.do_something();
+    }
+
+    public void start_server() throws Exception {
+        this.exampleServer = new ExampleServer();
+        this.exampleServer.startup().get();
     }
 
     public void do_something(){
         OpcUaClient client = null;
         try {
-            client = OpcUaClient.create("opc.tcp://localhost");
+            client = OpcUaClient.create(this.eUrl);
             client.connect().get();
 
             UaVariableNode node = client.getAddressSpace().getVariableNode(Identifiers.Server_ServerStatus_StartTime);
